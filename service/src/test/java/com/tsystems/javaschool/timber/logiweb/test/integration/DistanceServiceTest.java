56e52a14d17f5cc268f8549915cb3a2d460beceb
timur <timur.salakhetdinov@gmail.com>
2016-02-27 04:14:53 +0300

    divided into 3 maven modules: persistence, service, view

---
+++ service/src/test/java/com/tsystems/javaschool/timber/logiweb/test/integration/DistanceServiceTest.java
@@ -0,0 +1,34 @@
+package com.tsystems.javaschool.timber.logiweb.test.integration;
+
+import com.tsystems.javaschool.timber.logiweb.dao.jpa.CityDaoJpa;
+import com.tsystems.javaschool.timber.logiweb.dao.jpa.DistanceDaoJpa;
+import com.tsystems.javaschool.timber.logiweb.entity.City;
+import com.tsystems.javaschool.timber.logiweb.entity.Distance;
+import com.tsystems.javaschool.timber.logiweb.service.CityService;
+import com.tsystems.javaschool.timber.logiweb.service.DistanceService;
+import com.tsystems.javaschool.timber.logiweb.service.impl.CityServiceImpl;
+import com.tsystems.javaschool.timber.logiweb.service.impl.DistanceServiceImpl;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.List;
+
+/**
+ * Created by tims on 2/22/2016.
+ */
+public class DistanceServiceTest {
+
+    @Test
+    public void FindAll() {
+        DistanceService distanceService = new DistanceServiceImpl(new DistanceDaoJpa(Distance.class));
+        List<Distance> distances = distanceService.findAll();
+        CityService cityService = new CityServiceImpl(new CityDaoJpa(City.class));
+        List<City> cities = cityService.findAll();
+        int numOfCities = cities.size();
+        // in full directed graph G(V,E) |E| = |V|*(|V|-1)
+        // also we need zero routes from city to itself
+        // to easy calculate route length
+        int numOfDistances = numOfCities * (numOfCities - 1);
+        Assert.assertEquals(numOfDistances + numOfCities, distances.size());
+    }
+}
