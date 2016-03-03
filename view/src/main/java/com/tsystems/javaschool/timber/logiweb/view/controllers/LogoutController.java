63253594b297232c7e25ca3a76e6fbd8a3b836b2
Timur <timur.salakhetdinov@gmail.com>
2016-03-03 05:17:20 +0300

    config log4j to log each module to separate log files

--- view/src/main/java/com/tsystems/javaschool/timber/logiweb/view/controllers/LogoutController.java
+++ view/src/main/java/com/tsystems/javaschool/timber/logiweb/view/controllers/LogoutController.java
@@ -1,10 +1,14 @@
 package com.tsystems.javaschool.timber.logiweb.view.controllers;
 
+import org.apache.log4j.Logger;
+import org.joda.time.DateTime;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Date;
 
 /**
  * Servlet implementation class Test
@@ -12,6 +16,7 @@ import java.io.IOException;
 public class LogoutController extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
+    final static Logger logger = Logger.getLogger(LogoutController.class);
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -24,6 +29,13 @@ public class LogoutController extends HttpServlet {
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username = request.getUserPrincipal().getName();
+        String userrole = "";
+        if (request.isUserInRole("manager"))
+            userrole = "manager";
+        else
+            userrole = "driver";
+        logger.info("User: " + username + " in role: " + userrole + " logged out at " + new Date());
         request.getSession().invalidate();
         response.sendRedirect(getServletContext().getContextPath() + "/home.jsp");
     }
